356b6f4f3851ef759d3fe8f1ba34d99e61c3a779
Yang Li <liyang0525@hotmail.com>
2020-02-14 13:04:53 -0800

    change location

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+   $("#test").click(function() {
+        $("#endcase-wrap").fadeIn();
+   }); 
+
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# Osaka
+The website demonstrate my 17 days highlight while I was travlleling in Japan.
